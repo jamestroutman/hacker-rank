242f2f33fb397ba2cd0783cc5c015f9631434110
qrails-jtroutman <jtroutman@qrails.com>
2023-08-07 14:28:36 -0500

    Plus Minus

---
+++ Plus Minus/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.plusMinus(arr);
+        }
+
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'plusMinus' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void plusMinus(List<int> arr)
+        {
+            float positives = arr.Count(x => x > 0);
+            float negatives = arr.Count(x => x < 0);
+            float zeros = arr.Count(x => x == 0);
+            float count = arr.Count();
+
+            double positiveRate = positives / count;
+            Console.WriteLine(positiveRate.ToString("0.000000"));
+
+            double negativeRate = negatives / count;
+            Console.WriteLine(negativeRate.ToString("0.000000"));
+
+            double zeroRate = zeros / count;
+            Console.WriteLine(zeroRate.ToString("0.000000"));
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 - Counting Vallleys 15/15
 - Jumping in the Clouds 20/20
 - Diagonal Difference 10/10
+- Plus Minus 10/10
 
 dotnet new console -o "XXXX"
